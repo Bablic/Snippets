75d64612fafff3ba1d3897ca3c45c5e8aa576b71
Ishai Jaffe <receipts@bablic.com>
2017-01-05 12:51:15 +0200

    Create AutoDetectInCustomDomains.js

---
+++ AutoDetectInCustomDomains.js
@@ -0,0 +1,44 @@
+
+	var detectBrowserLocale = function () {
+		var language = navigator.userLanguage || navigator.language;
+		if (!language)
+			return null;
+		return language.split('-')[0];
+	};
+
+	var bablicRedirect = function() {
+    // dont redirect for crawlers
+		var isBot = navigator && navigator.userAgent && /bot|crawler|baiduspider|facebookexternalhit|Twitterbot|80legs|mediapartners-google|adsbot-google|seo/i.test(navigator.userAgent);
+		if (isBot)
+			return;
+
+		document.addEventListener('bablicload',function() {
+			var locale = detectBrowserLocale();
+			if(!locale) return;
+			var domain = bablic.data.customUrls[locale];
+			if(!domain) return;
+			domain = domain.split('/')[0];
+			if (location.hostname!=domain){
+         // redirect is needed, only if url not explictly says locale, or cookie says
+				var currentSearch = location.search;
+				if (currentSearch && currentSearch.indexOf('locale=') > -1)
+					return;
+				if (document.cookie && document.cookie.indexOf('bab_locale') > -1)
+					return;
+
+				location.href = location.protocol + '//'+ domain + location.pathname + currentSearch;				
+			}
+			else {
+          // before user changes a language, make sure locale string is a part of the URL (to prevent back redirects)
+			    bablic.on('beforeLocaleChange',function(targetLocale){
+					var targetHref = location.pathname + location.search + location.hash;					
+					if(targetHref.indexOf('?') > -1)
+						targetHref += '&locale=' + targetLocale;
+					else
+						targetHref += '?locale=' + targetLocale;
+					history.replaceState(null,null,targetHref);
+				});
+			}			
+		});
+	};
+	bablicRedirect();
